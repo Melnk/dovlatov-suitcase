43f50f20fe5e5c9e5d812c76343f5f727548d204
Melnik <melnikoleg080@gmail.com>
2025-11-02 22:08:00 +0300

    Добавлен класс для общего вхождения и подключения к БД

--- src/main/java/ru/dovlatov/suitcase/TestConnection.java
+++ src/main/java/ru/dovlatov/suitcase/TestConnection.java
@@ -1,4 +1,17 @@
 package ru.dovlatov.suitcase;
 
+import ru.dovlatov.suitcase.config.ConnectionManager;
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class TestConnection {
+    public static void main(String[] args) {
+        try (Connection conn = ConnectionManager.getConnection()) {
+            if (conn != null && !conn.isClosed()) {
+                System.out.println("Connection established");
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
--- src/main/java/ru/dovlatov/suitcase/config/ConnectionManager.java
+++ src/main/java/ru/dovlatov/suitcase/config/ConnectionManager.java
@@ -1,7 +1,12 @@
 package ru.dovlatov.suitcase.config;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
 
 public class ConnectionManager {
     private static final String PROPERTIES_FILE = "db.properties";
@@ -9,11 +14,26 @@ public class ConnectionManager {
     private static String username;
     private static String password;
 
+    public ConnectionManager() {
+    }
+
     static {
         try (InputStream input = ConnectionManager.class.getClassLoader().getResourceAsStream(PROPERTIES_FILE)) {
-
-        } catch (IOException e) {
+            Properties props = new Properties();
+            if (input == null) {
+                throw new FileNotFoundException("Файл " + PROPERTIES_FILE + " не найден");
+            }
+            props.load(input);
+            url = props.getProperty("db.url");
+            username = props.getProperty("db.username");
+            password = props.getProperty("db.password");
+            Class.forName(props.getProperty("db.driver"));
+        } catch (IOException | ClassNotFoundException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(url, username, password);
     }
 }
